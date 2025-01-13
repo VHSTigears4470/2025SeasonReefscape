8e92d72020582726ed517131a70dcda38cdae565
phomebrew <stevenhsiao4@gmail.com>
2025-01-13 01:04:41 -0800

    Imported Swerve Code Used Code from Rev to Code Swerve     WARNING : NOT TESTED YET AND IDs AND CHASIS SIZE STILL NEED TO BE CONFIG Added additional code for debugging Git Repo : https://github.com/REVrobotics/MAXSwerve-Java-Template

--- 2025SeasonReefscape/src/main/java/frc/robot/RobotContainer.java
+++ 2025SeasonReefscape/src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,17 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.TeleConstants;
+import frc.robot.commands.Drivetrain.SwerveJoystickCommand;
+import frc.robot.commands.Drivetrain.TestDrivingMotors;
+import frc.robot.commands.Drivetrain.TestSetPosCommand;
+import frc.robot.commands.Drivetrain.TestTurningMotors;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -19,15 +25,23 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
+  DriveSubsystem m_driveSub = new DriveSubsystem();
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      new CommandXboxController(OIConstants.k_DriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_driveSub
+    .setDefaultCommand(new SwerveJoystickCommand(
+        m_driveSub, 
+        () -> m_driverController.getRawAxis(OIConstants.k_driverAxisY),
+        () -> m_driverController.getRawAxis(OIConstants.k_driverAxisX),
+        () -> m_driverController.getRawAxis(OIConstants.k_driverAxisRot),
+        () -> true,
+        "Default / Field Oriented"
+      )
+    );
     // Configure the trigger bindings
     configureBindings();
   }
@@ -42,13 +56,18 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    int preset = 2;
+    switch (preset) {
+            case 0: 
+                    controllerPresetMain();
+                    break;
+            case 1:
+                    controllerPresetOne();
+                    break;
+            default:
+                    controllerPresetMain();
+                    break;
+    }
   }
 
   /**
@@ -57,7 +76,233 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
+  }
+
+  // Presets
+  /**
+   * The default / main preset used for comps
+   * Does the following: 
+   * nothing for now :D
+   */
+  public void controllerPresetMain() {
+
+  }
+
+  /**
+   * The default / main preset used for comps
+   * Does the following: 
+   *    Button B : Reset Swerve's Odometer 
+   *    Button A : Stops All Swerve Modules
+   *    Button X : Make Wheels Straight
+   *    Right Trigger : Hold + Joystick w/o being field orientated
+   *    Right Bumper : Hold to go forward relative to robot
+   *    Left Trigger : Rotate Clckwise
+   *    Left Bumper : Rotate Counter Clockwise
+   */
+  public void controllerPresetOne() {
+        // Reset Odom
+        m_driverController.b().onTrue(
+                new SequentialCommandGroup(
+                        new InstantCommand(
+                                ()-> m_driveSub.zeroHeading(),
+                                m_driveSub
+                        ),
+                        new InstantCommand(
+                                ()-> m_driveSub.resetOdometry(new Pose2d()),
+                                m_driveSub
+                        )
+                )
+        );
+
+        // Stop Button
+        m_driverController.a().onTrue(
+                new InstantCommand(
+                        () -> m_driveSub.stopModules(), 
+                        m_driveSub
+                )
+        );
+
+        // Straighten Wheels
+        m_driverController.x().whileTrue(
+                new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (boolean) false,
+                        "Wheels Straight"
+                )
+        );
+
+
+        // drive with Robot Orientation
+        m_driverController.rightTrigger().whileTrue(
+                new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> m_driverController.getRawAxis(OIConstants.k_driverAxisY),
+                        () -> m_driverController.getRawAxis(OIConstants.k_driverAxisX),
+                        () -> m_driverController.getRawAxis(OIConstants.k_driverAxisRot),
+                        // When pressed, changes to robot orientated
+                        () -> false,
+                        "Robot Orientated"
+                )
+        );
+
+        // drive forward only
+        m_driverController.rightBumper().whileTrue(
+                new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) 0.4,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (boolean) false,
+                        "Forward"
+                )
+        );
+
+        // rotate clockwise with joystick input
+        m_driverController.leftBumper().whileTrue(
+                new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (double) 0.4,
+                        () -> (boolean) false,
+                        "Rotate"
+                )
+        );
+
+        // rotate counter clockwise with joystick input
+        m_driverController.leftTrigger().whileTrue(
+                new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (double) -0.4,
+                        () -> (boolean) false,
+                        "Rotate"
+                )
+        );
+
+  }
+
+  /**
+   * The default / main preset used for comps
+   * Does the following: 
+   *    Right Trigger : Reset Heading & Odom
+   *    Right Bumper : Rotate Wheels Clockwise
+   *    Left Bumper : Rotate Wheels Clockwise
+   * 
+   *    Button B : Set Wheels Right
+   *    Button X : Set Wheels Left
+   *    Button Y : Set Wheel Straight
+   *    Button A : Set Wheels Backward
+   *    
+   *    Left Trigger + Button B : Move Robot Right
+   *    Left Trigger + Button X : Move Robot Left
+   *    Left Trigger + Button Y : Move Robot Forward
+   *    Left Trigger + Button A : Move Robot Backward
+   * 
+   *    Start Button : Move Wheels In Whatever Direction they're facing at 10% max speed
+   */
+  public void controllerPresetTwo() {
+        // Reset odom, reset encoders, go to 0 pos (turn), turn, 
+        // Reset pos = > zero gyro -> reset odom
+        // turn (idk why )
+        m_driverController.rightTrigger().onTrue(
+                new SequentialCommandGroup(
+                        new InstantCommand(
+                                ()-> m_driveSub.zeroHeading(),
+                                m_driveSub
+                        ),
+                        new InstantCommand(
+                                ()-> m_driveSub.resetOdometry(new Pose2d()),
+                                m_driveSub
+                        )
+                )
+        );
+
+        // right movement
+        m_driverController.b().and(m_driverController.leftTrigger()).whileTrue(
+                    new SwerveJoystickCommand(
+                    m_driveSub,
+                    () -> (double) 0.0,
+                    () -> (double) 0.4,
+                    () -> (double) 0.0,
+                    () -> (boolean) false,
+                    "Right Movement"
+                )
+        );
+
+        // right rotation
+        m_driverController.b().and(m_driverController.leftTrigger().negate()).whileTrue(
+                new TestSetPosCommand(m_driveSub, Math.PI * 0.5)
+        );
+
+        // left movement
+        m_driverController.x().and(m_driverController.leftTrigger()).whileTrue(
+                        new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) 0.0,
+                        () -> (double) -0.4,
+                        () -> (double) 0.0,
+                        () -> (boolean) false,
+                        "Left Movement"
+                )
+        );
+
+        // left rotation
+        m_driverController.x().and(m_driverController.leftTrigger().negate()).whileTrue(
+                new TestSetPosCommand(m_driveSub, Math.PI * 1.5)
+        );
+
+        // forwards movement
+        m_driverController.y().and(m_driverController.leftTrigger()).whileTrue(
+                        new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) 0.4,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (boolean) false,
+                        "Forward Movement"
+                )
+        );
+
+        // forwards rotation
+        m_driverController.y().and(m_driverController.leftTrigger().negate()).whileTrue(
+                new TestSetPosCommand(m_driveSub, 0)
+        );
+
+        // backwards movement
+        m_driverController.a().and(m_driverController.leftTrigger()).whileTrue(
+                        new SwerveJoystickCommand(
+                        m_driveSub,
+                        () -> (double) -0.4,
+                        () -> (double) 0.0,
+                        () -> (double) 0.0,
+                        () -> (boolean) false,
+                        "Backward Movement"
+                )
+        );
+
+        // backwards rotation
+        m_driverController.a().and(m_driverController.leftTrigger().negate()).whileTrue(
+                new TestSetPosCommand(m_driveSub, Math.PI)
+        );
+
+        // Rotate wheels clockwise
+        m_driverController.rightBumper().whileTrue(
+                new TestTurningMotors(m_driveSub, true)
+        );
+
+        // Rotate wheels counterclockwise
+        m_driverController.leftBumper().whileTrue(
+                new TestTurningMotors(m_driveSub, false)
+        );
+
+        m_driverController.start().whileTrue(
+                new TestDrivingMotors(m_driveSub, 0.10 * TeleConstants.k_MaxSpeedMetersPerSecond)
+        );
   }
 }
---
+++ 2025SeasonReefscape/src/main/java/frc/robot/commands/Drivetrain/TestDrivingMotors.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.Drivetrain;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class TestDrivingMotors extends Command {
+    private final DriveSubsystem swerveSubsystem;
+    private final double speed;
+
+    /**
+     * Contructs a Command to continously rotate only the wheels
+     * @param swerveSubsystem subsystem that controls the swerve
+     * @param moveClockwise whether or not the robot wheels should move clockwise
+     */
+    public TestDrivingMotors(DriveSubsystem swerveSubsystem, double speed) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.speed = speed;
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        SmartDashboard.putString("Drive Mode", "Driving Wheels"); // Helps understand which command swerve drive is using
+    }
+
+    @Override
+    public void execute() {
+        swerveSubsystem.testDriveMotors(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerveSubsystem.stopModules();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
