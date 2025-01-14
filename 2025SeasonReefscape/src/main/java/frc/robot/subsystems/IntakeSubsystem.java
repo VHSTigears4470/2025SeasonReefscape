bea0fca94ce86784a2cb3fb23184ac70b2c9c85c
NahtanH <nate.k.huynh@gmail.com>
2025-01-13 17:36:52 -0800

    IntakeSubsystem Declared instances, renamed values to fit conventions

--- 2025SeasonReefscape/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ 2025SeasonReefscape/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,12 +4,39 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
+  private final SparkMax m_intakeMotorLeft;
+  private final SparkMax m_intakeMotorRight;
+  private final SparkMax m_shootMotorLeft;
+  private final SparkMax m_shootMotorRight;
+
+  private final RelativeEncoder m_intakeEncoderLeft;
+  private final RelativeEncoder m_intakeEncoderRight;
+  private final RelativeEncoder m_shootEncoderLeft;
+  private final RelativeEncoder m_shootEncoderRight;
+
+  private final AnalogInput sensor; //TODO: Change datatype
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
-    
+    //TODO: Add channels in constants
+    m_intakeMotorLeft = new SparkMax(0, MotorType.kBrushless);
+    m_intakeMotorRight = new SparkMax(0, MotorType.kBrushless);
+    m_shootMotorLeft = new SparkMax(0, MotorType.kBrushless);
+    m_shootMotorRight = new SparkMax(0, MotorType.kBrushless);
+
+    m_intakeEncoderLeft = m_intakeMotorLeft.getEncoder();
+    m_intakeEncoderRight = m_intakeMotorRight.getEncoder();
+    m_shootEncoderLeft= m_shootMotorLeft.getEncoder();
+    m_shootEncoderRight = m_shootMotorRight.getEncoder();
+
+    sensor = new AnalogInput(0);
   }
 
 
