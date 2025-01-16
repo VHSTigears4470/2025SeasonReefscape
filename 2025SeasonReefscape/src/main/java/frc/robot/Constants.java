44eaa0324cd6819ee5329db5daf36340c592019d
NahtanH <nate.k.huynh@gmail.com>
2025-01-15 18:36:57 -0800

    IntakeSubsystem Updated formatting TODO: Replace SparkMax(s) with CANSparkMax(s), replace RelativeEncoder(s) with SparkMaxRelativeEncoder(s),       setPositionConversionFactor

--- 2025SeasonReefscape/src/main/java/frc/robot/Constants.java
+++ 2025SeasonReefscape/src/main/java/frc/robot/Constants.java
@@ -22,6 +22,8 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final double k_sparkMaxConversionFactor = 0; //TODO: Update
+
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
@@ -132,9 +134,9 @@ public final class Constants {
   }
 
   public static final class IntakeConstants {
-    public static final int k_intakePortLeft = 3;
-    public static final int k_intakePortRight = 1;
-    public static final int k_shootPortLeft = 2;
-    public static final int k_shootPortRight = 0;
+    public static final int k_intakePortLeft = 1;
+    public static final int k_intakePortRight = 2;
+    public static final int k_shootPortLeft = 3;
+    public static final int k_shootPortRight = 4;
   }
 }
--- 2025SeasonReefscape/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ 2025SeasonReefscape/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,41 +6,40 @@ package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkRelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private final SparkMax m_intakeMotorLeft; //Back Motor
-  private final SparkMax m_intakeMotorRight; //Back Motor
-  private final SparkMax m_shootMotorLeft; // Front Motor
-  private final SparkMax m_shootMotorRight; // Front Motor
+  //Intake - Back, Shoot - Front
+  private final SparkMax m_intakeMotorLeft; 
+  private final SparkMax m_intakeMotorRight; 
+  private final SparkMax m_shootMotorLeft; 
+  private final SparkMax m_shootMotorRight; 
 
   private final RelativeEncoder m_intakeEncoderLeft;
   private final RelativeEncoder m_intakeEncoderRight;
   private final RelativeEncoder m_shootEncoderLeft;
   private final RelativeEncoder m_shootEncoderRight;
 
-  private final AnalogInput sensor; //TODO: Change datatype
+  private final AnalogInput m_sensor; //TODO: Change datatype as applicable
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
-    //TODO: Add channels in constants
-    m_intakeMotorLeft = new SparkMax(0, MotorType.kBrushless);
-    m_intakeMotorRight = new SparkMax(0, MotorType.kBrushless);
-    m_shootMotorLeft = new SparkMax(0, MotorType.kBrushless);
-    m_shootMotorRight = new SparkMax(0, MotorType.kBrushless);
+    m_intakeMotorLeft = new SparkMax(Constants.IntakeConstants.k_intakePortLeft, MotorType.kBrushless);
+    m_intakeMotorRight = new SparkMax(Constants.IntakeConstants.k_intakePortRight, MotorType.kBrushless);
+    m_shootMotorLeft = new SparkMax(Constants.IntakeConstants.k_shootPortLeft, MotorType.kBrushless);
+    m_shootMotorRight = new SparkMax(Constants.IntakeConstants.k_shootPortRight, MotorType.kBrushless);
 
-  //TODO SET MOTOR DEFAULT 
-
-    //TODO;CHECK IF INTAKE IS ON
     m_intakeEncoderLeft = m_intakeMotorLeft.getEncoder();
     m_intakeEncoderRight = m_intakeMotorRight.getEncoder();
     m_shootEncoderLeft= m_shootMotorLeft.getEncoder();
     m_shootEncoderRight = m_shootMotorRight.getEncoder();
 
-    sensor = new AnalogInput(0);
+    m_sensor = new AnalogInput(0); //TODO: Add constant for port if applicable
   }
 
  public double getIntakeEncoderLeft(){
@@ -61,13 +60,17 @@ public double getShootEncoderLeft(){
 
  public AnalogInput SensorValue(){
   // COME BACK TO THIS LATER: WE DONT KNOW WHAT SENSOR WE ARE USING YET
-  return sensor;
+  return m_sensor;
  }
 
- public void SmartDashboard() {
+ public void setSmartDashboard(){
   SmartDashboard.putNumber("ShootEncoderL", m_shootEncoderLeft.getPosition() );
   SmartDashboard.putNumber("ShootEncoderR", m_shootEncoderRight.getPosition() );
   SmartDashboard.putNumber("IntakeEncoderL", m_intakeEncoderLeft.getPosition() );
   SmartDashboard.putNumber("IntakeEncoderL", m_intakeEncoderRight.getPosition() );
+ }
+
+ public void periodic(){
+  setSmartDashboard();
  }
 }
