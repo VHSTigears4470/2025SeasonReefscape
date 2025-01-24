5928bc469079a6564b23e630de5b90a6e0b0436f
AlexPinkxXx <131723653+AlexPinkxXx@users.noreply.github.com>
2025-01-24 15:51:50 -0800

    Algaeshoot

--- 2025SeasonReefscape/src/main/java/frc/robot/commands/AlgaeCommands/ShootAlgae.java
+++ 2025SeasonReefscape/src/main/java/frc/robot/commands/AlgaeCommands/ShootAlgae.java
@@ -4,16 +4,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.AlgaeConstants.ALGAE_ARM_STATE;
 import frc.robot.Constants.IntakeConstants.ARM_STATE;
 import frc.robot.subsystems.AlgaeSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.CoralSubsystem;
 
 public class ShootAlgae extends Command{
     private final AlgaeSubsystem m_algaeSub;
     private final ALGAE_ARM_STATE desiredReferencePosition;
 
-    public ShootAlgae  (AlgaeSubsystem algaeSub, IntakeSubsystem intakeSystem)
-     m_algaeSub = algaeSub;
-     addRequirements(algaeSub);
-{
-  
-}
+    public ShootAlgae (AlgaeSubsystem algaeSub) {
+        m_algaeSub = algaeSub;
+        addRequirements(algaeSub);
+    }
 }
