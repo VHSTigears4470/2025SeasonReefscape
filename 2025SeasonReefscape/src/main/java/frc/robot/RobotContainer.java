c38704c57685c8820ea26d8e6b44163636f0ceba
phomebrew <stevenhsiao4@gmail.com>
2025-01-26 23:27:47 -0800

    PathPlanner Configs
    Created some demo path planner paths to use

--- 2025SeasonReefscape/src/main/java/frc/robot/RobotContainer.java
+++ 2025SeasonReefscape/src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,9 @@ import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.CoralSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.KitbotDriveSubsystem;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -113,7 +116,10 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return null;
+        if(UsingConstants.k_usingSwerveDrive) {
+                return new PathPlannerAuto("Straight Auto");
+        }
+        return null;
   }
 
   // Presets
